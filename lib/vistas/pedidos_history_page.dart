672331173a72bcc020a51765d965ca274382d1a6
DwinR6 <e_noel-r.r96@hotmail.com>
2020-10-31 22:38:17 -0600

    Diseño de perfil_page, added edit_pages + history

---
+++ lib/vistas/pedidos_history_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PedidosHistory extends StatelessWidget {
+  const PedidosHistory({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/vistas/perfil_page.dart
+++ lib/vistas/perfil_page.dart
@@ -1,4 +1,7 @@
 import 'package:deliveryapplicacion/recursos/recursos.dart';
+import 'package:deliveryapplicacion/vistas/edit_pass_page.dart';
+import 'package:deliveryapplicacion/vistas/edit_phone_page.dart';
+import 'package:deliveryapplicacion/vistas/pedidos_history_page.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,61 +11,58 @@ class PerfilPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: size.width * 0.2),
-        child: SingleChildScrollView(
-          child: Row(
+        body: Container(
+      margin: EdgeInsets.only(top: size.width * 0.2),
+      child: Column(
+        children: [
+          _titulo(),
+          SizedBox(height: size.height * 0.02),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    _titulo(),
-                    SizedBox(height: size.height * 0.02),
-                    _icono(size),
-                    SizedBox(height: size.height * 0.02),
-                    _saludo(),
-                    SizedBox(height: size.height * 0.02),
-                    _saludo2(),
-                    SizedBox(height: size.height * 0.05),
-                    //_body(size)
-                    _botondirecciones(size),
-                    SizedBox(height: size.height * 0.05),
-                    _botontelefono(size),
-                  ],
-                ),
+              _icono(size),
+              SizedBox(width: size.height * 0.02),
+              Column(
+                children: [
+                  SizedBox(height: size.height * 0.02),
+                  _saludo(),
+                  SizedBox(height: size.height * 0.02),
+                  _saludo2(),
+                  SizedBox(height: size.height * 0.02),
+                  _saludo3()
+                  //_body(size)
+                ],
               ),
             ],
           ),
-        ),
+          SizedBox(height: size.height * 0.05),
+          _options(context)
+        ],
       ),
-    );
+    ));
   }
-
 
   Widget _titulo() {
     final style = TextStyle(
         fontWeight: FontWeight.w900,
-        fontSize: 29,
+        fontSize: 25,
         color: Recursos().colorPrimario);
     return Text('Mi Perfil', style: style);
   }
 
   Widget _saludo() {
-    final style = TextStyle(
-        fontWeight: FontWeight.w900,
-        fontSize: 25,
-        color: Recursos().colorPrimario);
+    final style = TextStyle(fontSize: 25.0, color: Recursos().colorPrimario);
     return Text('Hola', style: style);
   }
 
   Widget _saludo2() {
-    final style = TextStyle(
-        fontWeight: FontWeight.w900,
-        fontSize: 25,
-        color: Recursos().colorPrimario);
+    final style = TextStyle(color: Recursos().colorPrimario);
     return Text('Bienvenidos a El Cipitillo ', style: style);
+  }
+
+  Widget _saludo3() {
+    final style = TextStyle(color: Recursos().colorPrimario);
+    return Text('+503 7861 9772 ', style: style);
   }
 
   Widget _icono(Size size) {
@@ -78,7 +78,7 @@ class PerfilPage extends StatelessWidget {
       ]),
       child: CircleAvatar(
         radius: size.width * 0.13,
-        backgroundColor: Recursos().colorPrimario,
+        backgroundColor: Recursos().colorTerciario,
         child: Text('EC'),
       ),
     );
@@ -127,6 +127,92 @@ class PerfilPage extends StatelessWidget {
         textColor: Colors.white,
         splashColor: Recursos().colorSecundario,
       ),
+    );
+  }
+
+  Widget _options(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 35, horizontal: 10.0),
+      child: Column(
+        children: [
+          Divider(color: Recursos().colorPrimario),
+          _telefono(context),
+          Divider(
+            color: Recursos().colorPrimario,
+          ),
+          _clave(context),
+          Divider(
+            color: Recursos().colorPrimario,
+          ),
+          _historial(context),
+          Divider(
+            color: Recursos().colorPrimario,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _telefono(BuildContext context) {
+    return ListTile(
+      title: Container(
+        child: Text(
+          'Editar mi numero de telefono',
+          style: TextStyle(
+            color: Recursos().colorPrimario,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      leading: Icon(Icons.phone_android, color: Recursos().colorPrimario),
+      trailing: Icon(Icons.chevron_right, color: Recursos().colorPrimario),
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => EditPhone()));
+      },
+    );
+  }
+
+  Widget _clave(BuildContext context) {
+    return ListTile(
+      title: Container(
+        child: Text(
+          'Cambiar contraseña',
+          style: TextStyle(
+            color: Recursos().colorPrimario,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      leading: Icon(Icons.vpn_key, color: Recursos().colorPrimario),
+      trailing: Icon(Icons.chevron_right, color: Recursos().colorPrimario),
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => EditPass()));
+      },
+    );
+  }
+
+  Widget _historial(BuildContext context) {
+    return ListTile(
+      title: Container(
+        child: Text(
+          'Historial de pedidos',
+          style: TextStyle(
+            color: Recursos().colorPrimario,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      leading: Icon(Icons.list_alt, color: Recursos().colorPrimario),
+      trailing: Icon(Icons.chevron_right, color: Recursos().colorPrimario),
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => PedidosHistory()));
+      },
     );
   }
 
