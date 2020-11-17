e9c2558cced18accd0a44b7b1e9c1ef0cd24afae
DwinR6 <e_noel-r.r96@hotmail.com>
2020-11-17 05:32:37 -0600

    Cambio de promo_model

--- lib/modelos/promo_model.dart
+++ lib/modelos/promo_model.dart
@@ -1,27 +1,68 @@
 import 'dart:convert';
 
-import 'menu_model.dart';
 
+class Promos {
+
+  List<Promocion> items = new List();
+
+  Promos();
+
+  Promos.fromJsonList( List<dynamic> jsonList  ) {
+
+    if ( jsonList == null ) return;
+
+    for ( var item in jsonList  ) {
+      final direccion = new Promocion.fromJson(item);
+      items.add( direccion );
+    }
+
+  }
+
+}
 Promocion promocionFromJson(String str) => Promocion.fromJson(json.decode(str));
 
 String promocionToJson(Promocion data) => json.encode(data.toJson());
 
 class Promocion {
-  Promocion({
-    this.idPromo,
-    this.menu,
-  });
+    Promocion({
+        this.idPromo,
+        this.menus,
+        this.titulo,
+        this.tipo,
+        this.descripcion,
+        this.precio,
+        this.descuento,
+        this.imagen,
+    });
 
-  int idPromo;
-  Menu menu;
+    String idPromo;
+    List<dynamic> menus;
+    String titulo;
+    String tipo;
+    String descripcion;
+    double precio;
+    double descuento;
+    String imagen;
 
-  factory Promocion.fromJson(Map<String, dynamic> json) => Promocion(
-        idPromo: json["idPromo"],
-        menu: Menu.fromJson(json["menu"]),
-      );
+    factory Promocion.fromJson(Map<String, dynamic> json) => Promocion(
+        idPromo: json["id_promo"],
+        menus: List<dynamic>.from(json["menus"].map((x) => x)),
+        titulo: json["titulo"],
+        tipo: json["tipo"],
+        descripcion: json["descripcion"],
+        precio: json["precio"],
+        descuento: json["descuento"],
+        imagen: json["imagen"],
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "idPromo": idPromo,
-        "menu": menu.toJson(),
-      };
+        "menus": List<dynamic>.from(menus.map((x) => x)),
+        "titulo": titulo,
+        "tipo": tipo,
+        "descripcion": descripcion,
+        "precio": precio,
+        "descuento": descuento,
+        "imagen": imagen,
+    };
 }
